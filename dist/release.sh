9d8bc933bc49575663010e797ef6787a5e3f7d43
Julien Kohler <kohlerj@users.noreply.github.com>
2024-11-01 16:18:52 +0100

    Update release.sh
    Signed-off-by: Julien Kohler <kohlerj@users.noreply.github.com>

--- dist/release.sh
+++ dist/release.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 [[ ! -f "package.json" ]] && echo "Please run this script from the root of Duet Web Control!" && exit 1
-exit 0
 
 npm install
 npm run build-plugin MillenniumOS_UI
