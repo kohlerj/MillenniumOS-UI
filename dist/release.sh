9d42b2626db07d4070f99b6a74c3839ee4474c6f
Julien Kohler <kohlerj@users.noreply.github.com>
2024-11-01 16:08:59 +0100

    Automatic versioning

--- dist/release.sh
+++ dist/release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 [[ ! -f "package.json" ]] && echo "Please run this script from the root of Duet Web Control!" && exit 1
+exit 0
 
 npm install
 npm run build-plugin MillenniumOS
